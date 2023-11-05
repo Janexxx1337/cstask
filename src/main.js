28c710aa2200222172b26dc1c07220277adc0672
235 <krahs@gmai.com>
2023-11-05 17:23:35 +0300

    doneVersion

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// main.ts
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// PrimeVue стили
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.mount('#app');
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path'; // Импортируйте модуль path
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Изменение здесь
+    },
+    // Добавьте .ts и .tsx расширения для резолва модулей
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  // ... другие настройки
+});
